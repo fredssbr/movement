6ac977b860a2f5dcd32deb57947d45f750af38b8
Frederico Silva <p000fresilva@prservicos.com.br>
2019-09-16 14:35:50 -0300

    Mouse delay

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ var minIntervalInSeconds = 9;
 var maxIntervalInSeconds = 11;
 var actual = minIntervalInSeconds;
 var myFunction;
+var delay = 2000;
 
 function execute() {
     console.log("Actual interval: " + actual + " - Actual time: " + new Date().toISOString());
     actual = actual == minIntervalInSeconds ? maxIntervalInSeconds : minIntervalInSeconds;
+    console.log('First click: ' + new Date().toISOString());
+    robot.mouseClick();
+    robot.setMouseDelay(delay);
+    console.log('Second click: ' + new Date().toISOString());
     robot.mouseClick();
     clearInterval(myFunction);
     myFunction = setInterval(function(){
         execute();
-    }, actual * 60 * 1000);
+    }, ((actual * 60 * 1000) - delay));
  }
 
 myFunction = setInterval(function(){
